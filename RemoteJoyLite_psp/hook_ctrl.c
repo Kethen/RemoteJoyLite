f39a5e7214821c3512091915308c1b39bdbea4ac
Katharine Chui <katharine.chui@gmail.com>
2025-01-02 16:47:54 +0100

    rework sceCtrl buffer functions hooking for 6.xx

--- RemoteJoyLite_psp/hook_ctrl.c
+++ RemoteJoyLite_psp/hook_ctrl.c
@@ -85,6 +85,7 @@ static int MyCtrlBuffer( SceCtrlData *data, int count, int type )
 	return( ret );
 }
 
+#if 0
 /*------------------------------------------------------------------------------*/
 /* HookCtrlBufferFunction														*/
 /*------------------------------------------------------------------------------*/
@@ -94,7 +95,12 @@ static void HookCtrlBufferFunction( u32 *jump )
 	u32 inst   = _lw( target+8 );
 	u32 func   = (u32)MyCtrlBuffer;
 
-	if ( (inst & ~0x03FFFFFF) != 0x0C000000 ){ return; }
+	if ( (inst & ~0x03FFFFFF) != 0x0C000000 ){
+		#ifndef RELEASE
+		debug_printf("%s: failed hooking buffer function\n", __func__);
+		#endif
+		return;
+	}
 	sceCtrlBuffer_Func = (void *)GET_JUMP_TARGET( inst );
 	func = (func & 0x0FFFFFFF) >> 2;
 	_sw( 0x0C000000 | func, target+8 );
@@ -115,6 +121,7 @@ void hookCtrlBuffer( void )
 	HookCtrlBufferFunction( (u32 *)sceCtrlReadBufferNegative );
 	HookCtrlBufferFunction( (u32 *)sceCtrlPeekBufferNegative );
 }
+#endif
 
 /*******************************************************************************/
 /*------------------------------------------------------------------------------*/
@@ -174,17 +181,32 @@ static int MyCtrlReadLatch( SceCtrlLatch *latch )
 void hookCtrlLatch( void )
 {
 	SceModule *module = sceKernelFindModuleByName( "sceController_Service" );
-	if ( module == NULL ){ return; }
+	if ( module == NULL ){
+		#ifndef RELEASE
+		debug_printf("%s: failed fetching sceController_Service module\n", __func__);
+		#endif
+		return;
+	}
 
 	if ( sceCtrlPeekLatch_Func == NULL ){
 		sceCtrlPeekLatch_Func = HookNidAddress( module, "sceCtrl", 0xB1D0E5CD );
 		void *hook_addr = HookSyscallAddress( sceCtrlPeekLatch_Func );
+		#ifndef RELEASE
+		if(hook_addr == NULL){
+			debug_printf("%s: failed creating hook for sceCtrlPeekLatch\n", __func__);
+		}
+		#endif
 		HookFuncSetting( hook_addr, MyCtrlPeekLatch );
 	}
 
 	if ( sceCtrlReadLatch_Func == NULL ){
 		sceCtrlReadLatch_Func = HookNidAddress( module, "sceCtrl", 0x0B588501 );
 		void *hook_addr = HookSyscallAddress( sceCtrlReadLatch_Func );
+		#ifndef RELEASE
+		if(hook_addr == NULL){
+			debug_printf("%s: failed creating hook for sceCtrlReadLatch\n", __func__);
+		}
+		#endif
 		HookFuncSetting( hook_addr, MyCtrlReadLatch );
 	}
 }
@@ -202,4 +224,108 @@ void hookCtrlSetData( u32 PreData, u32 NowData, u32 Analog )
 	JoyLatch.uiPress   |=  NowData;
 	JoyLatch.uiRelease |= ~NowData;
 	LatchCount = 0;
+}
+
+typedef int (*CTRL_BUFFER_FUNC)(SceCtrlData *pad_data, int count);
+
+static CTRL_BUFFER_FUNC sceCtrlPeekBufferPositiveOrig = NULL;
+static CTRL_BUFFER_FUNC sceCtrlPeekBufferNegativeOrig = NULL;
+static CTRL_BUFFER_FUNC sceCtrlReadBufferPositiveOrig = NULL;
+static CTRL_BUFFER_FUNC sceCtrlReadBufferNegativeOrig = NULL;
+
+static int sceCtrlPeekBufferPositivePatched(SceCtrlData *pad_data, int count){
+	int ret = sceCtrlPeekBufferPositiveOrig(pad_data, count);
+	AddValues(pad_data, ret, 0);
+	return ret;
+}
+
+static int sceCtrlPeekBufferNegativePatched(SceCtrlData *pad_data, int count){
+	int ret = sceCtrlPeekBufferNegativeOrig(pad_data, count);
+	AddValues(pad_data, ret, 1);
+	return ret;
+}
+
+static int sceCtrlReadBufferPositivePatched(SceCtrlData *pad_data, int count){
+	int ret = sceCtrlReadBufferPositiveOrig(pad_data, count);
+	AddValues(pad_data, ret, 0);
+	return ret;
+}
+
+static int sceCtrlReadBufferNegativePatched(SceCtrlData *pad_data, int count){
+	int ret = sceCtrlReadBufferNegativeOrig(pad_data, count);
+	AddValues(pad_data, ret, 1);
+	return ret;
+}
+
+#define MAKE_JUMP(a, f) _sw(0x08000000 | (((u32)(f) & 0x0FFFFFFC) >> 2), a);
+#define GET_JUMP_TARGET(x) (0x80000000 | (((x) & 0x03FFFFFF) << 2))
+
+#define HIJACK_SYSCALL_STUB(a, f, ptr) { \
+	u32 _func_ = (u32)a; \
+	_func_ = GET_JUMP_TARGET(_lw(_func_)); \
+	u32 _ff = (u32)f; \
+	int _interrupts = pspSdkDisableInterrupts(); \
+	static u32 trampoline[3]; \
+	_sw(_lw(_func_), (u32)trampoline); \
+	_sw(_lw(_func_ + 4), (u32)trampoline + 8); \
+	MAKE_JUMP((u32)trampoline + 4, _func_ + 8); \
+	MAKE_JUMP(_func_, _ff); \
+	_sw(0, _func_ + 4); \
+	_sw((u32)trampoline, (u32)&ptr); \
+	sceKernelDcacheWritebackInvalidateAll(); \
+	sceKernelIcacheClearAll(); \
+	pspSdkEnableInterrupts(_interrupts); \
+}
+
+void hookCtrlBuffer( void ){
+	#if 0
+	SceModule *module = sceKernelFindModuleByName( "sceController_Service" );
+	if ( module == NULL ){
+		#ifndef RELEASE
+		debug_printf("%s: failed fetching sceController_Service module\n", __func__);
+		#endif
+		return;
+	}
+
+	// huh, why isn't nid hooking working on 6.xx
+	#define STR_(s) #s
+	#ifndef RELEASE
+	#define HOOK_LOG_(name) { \
+		if(name##Orig == NULL) { \
+			debug_printf("%s: failed fetching " STR_(name) " from nid\n", __func__); \
+		} \
+		if(hook_addr == NULL){ \
+			debug_printf("%s: failed creating hook for " STR_(name) "\n", __func__); \
+		} \
+	}
+	#else
+	#define HOOK_LOG_(name)
+	#endif
+
+	#define HOOK_(nid, name) { \
+		if(name##Orig == NULL) { \
+			name##Orig = HookNidAddress(module, "sceCtrl", nid); \
+			void *hook_addr = HookSyscallAddress(name##Orig); \
+			HOOK_LOG_(name); \
+			HookFuncSetting(hook_addr, name##Patched); \
+		} \
+	}
+
+	HOOK_(0x3A622550, sceCtrlPeekBufferPositive);
+	HOOK_(0xC152080A, sceCtrlPeekBufferNegative);
+	HOOK_(0x1F803938, sceCtrlReadBufferPositive);
+	HOOK_(0x60B81F86, sceCtrlReadBufferNegative);
+
+	#undef STR_
+	#undef HOOK_LOG_
+	#undef HOOK_
+
+	#else
+
+	HIJACK_SYSCALL_STUB(sceCtrlPeekBufferPositive, sceCtrlPeekBufferPositivePatched, sceCtrlPeekBufferPositiveOrig);
+	HIJACK_SYSCALL_STUB(sceCtrlPeekBufferNegative, sceCtrlPeekBufferNegativePatched, sceCtrlPeekBufferNegativeOrig);
+	HIJACK_SYSCALL_STUB(sceCtrlReadBufferPositive, sceCtrlReadBufferPositivePatched, sceCtrlReadBufferPositiveOrig);
+	HIJACK_SYSCALL_STUB(sceCtrlReadBufferNegative, sceCtrlReadBufferNegativePatched, sceCtrlReadBufferNegativeOrig);
+
+	#endif
 }
