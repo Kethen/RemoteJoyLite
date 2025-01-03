bd11448180b1b3bdb3b8288c736f68f05c673689
Katharine Chui <katharine.chui@gmail.com>
2025-01-03 13:03:42 +0100

    add dxvk to linux packaging

---
+++ linux_scripts/start_linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -xe
+script_dir=$(dirname "$0")
+script_dir=$(realpath "$script_dir")
+
+cd $script_dir;
+
+export WINEDLLOVERRIDES=d3d9=n,b
+
+MODE="JA"
+
+if [ -n "$(echo $0 | grep EN)" ]
+then
+	MODE="EN"
+fi
+
+EXE=RemoteJoyLite_${MODE}_32.exe
+
+if ! [ -e "$EXE" ]
+then
+	EXE=RemoteJoyLite_${MODE}_64.exe
+fi
+
+wine "$EXE"
